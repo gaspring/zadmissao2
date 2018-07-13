a411498414426d23046d5777d3ca248e8c6bddc9
barbara.watanabe@zellar.com.br <barbara.watanabe@zellar.com.br>
2018-07-13 16:56:37 -0300

    zoom done

--- lib/settings/api-settings.dart
+++ lib/settings/api-settings.dart
@@ -1,11 +1,11 @@
 class ApiSettings{
   // // DEV
-  // static String ENDPOINT = "http://192.168.30.61:6001/apis";
-  // static String ENDPOINT_NO_API = "http://192.168.30.61:6001";
+  static String ENDPOINT = "http://192.168.30.61:6001/apis";
+  static String ENDPOINT_NO_API = "http://192.168.30.61:6001";
 
   // PROD
-  static String ENDPOINT = "http://zellar2.azurewebsites.net/apis";
-  static String ENDPOINT_NO_API = "http://zellar2.azurewebsites.net";
+  // static String ENDPOINT = "http://zellar2.azurewebsites.net/apis";
+  // static String ENDPOINT_NO_API = "http://zellar2.azurewebsites.net";
 
   static String API_TOKEN = "API_TOKEN";
   static String ID_USER = "ID_USER";

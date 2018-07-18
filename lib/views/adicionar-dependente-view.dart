5a6bac6045764552f3d4f2848067c3b06ac950d9
barbara.watanabe@zellar.com.br <barbara.watanabe@zellar.com.br>
2018-07-18 12:36:35 -0300

    fix2

--- lib/views/adicionar-dependente-view.dart
+++ lib/views/adicionar-dependente-view.dart
@@ -149,12 +149,13 @@ class _AdicionarDependenteViewState extends State<AdicionarDependenteView> {
     return true;
   }
 
-  void _submit() async {
+      void _submit() {
     var nome = _textEditingControllerNome.text;
     var grauParentesco = _grauParentesco;
 
     if (!_validarNome(nome)) {
       _dialog.showAlertDialog("Ops...", "Digite o nome e sobrenome", "ok");
+      return;
     }
 
     if (!_validarGraParentesco(grauParentesco)) {
@@ -162,28 +163,25 @@ class _AdicionarDependenteViewState extends State<AdicionarDependenteView> {
     }
 
     _dialog.showProgressDialog();
-    var dependente = await _vagaService.adicionarDependente(
+    var dependente = _vagaService.adicionarDependente(
         new PreAdmissaoAppDependenteInput(
             idPreAdmissaoApp: widget.idPreAdmissaoApp,
             nome: nome,
             grauParentesco: grauParentesco));
-
     _dialog.dismiss();
 
-    if (dependente != null) {
-      //   Navigator.push(context, new MaterialPageRoute(builder: (context)=> new DependenteDocumentosView(preAdmissaoAppDependente: dependente, isNew: true)));
-      // } else {
+    if (dependente == null) {
       _dialog.showAlertDialog("Ops...", "Tente novamente", "ok");
     }
   }
 
-  //   void _submit() {
+  //   void _submit() async {
   //   var nome = _textEditingControllerNome.text;
   //   var grauParentesco = _grauParentesco;
 
   //   if (!_validarNome(nome)) {
   //     _dialog.showAlertDialog("Ops...", "Digite o nome e sobrenome", "ok");
-  //     return;
+    
   //   }
 
   //   if (!_validarGraParentesco(grauParentesco)) {
@@ -191,14 +189,16 @@ class _AdicionarDependenteViewState extends State<AdicionarDependenteView> {
   //   }
 
   //   _dialog.showProgressDialog();
-  //   var dependente = _vagaService.adicionarDependente(
+  //   var dependente = await _vagaService.adicionarDependente(
   //       new PreAdmissaoAppDependenteInput(
   //           idPreAdmissaoApp: widget.idPreAdmissaoApp,
   //           nome: nome,
   //           grauParentesco: grauParentesco));
-  //   _dialog.dismiss();
 
-  //   if (dependente == null) {
+  //   if (dependente != null) {
+  //     _dialog.dismiss();
+  //     Navigator.push(context, new MaterialPageRoute(builder: (context)=> new DependenteDocumentosView(preAdmissaoAppDependente: dependente, isNew: true)));
+  //   } else {
   //     _dialog.showAlertDialog("Ops...", "Tente novamente", "ok");
   //   }
   // }
